c50685bb0dbbcdd9a81d28b0a8f289083a47c985
Max Kirchoff <max@shopigniter.com>
2012-12-05 16:17:03 -0800

    added email alerts and functionalized everything

--- conf/conf.dist.php
+++ conf/conf.dist.php
@@ -6,8 +6,12 @@ return array(
         ),
     // how often do you want to ping services for their status
     'status_check_frequency'  =>  '{{status_check_frequency}}',
-    // enable audio alerts
-    'alerts'    => true,
+    // enable alert types
+    'alerts'    => array('email','audio'),
+    'alert_email'   =>  array(
+        'to'    => '{{alert_email_to}}',
+        'subject'   =>  '{{alert_email_subject}}'
+    ),
     // how often do you want to play alerts when they are unchanged (status check changes are immediatley sent)
     'alert_frequency'  =>  '{{alert_frequency}}',
     // set successful status alert wav or mp3 file path
@@ -16,4 +20,6 @@ return array(
     'alert_failure' => '{{alert_failure}}',
     // set message for when status has changed from failure to success
     'alert_restored' => '{{alert_restored}}',
+    // set the desired timezone
+    'timezone'  =>  '{{timezone}}',
 );
--- so_gnar.php
+++ so_gnar.php
@@ -1,96 +1,231 @@
 <?php
 require('api_thingy.php');
 
-// CONF
-if (! file_exists('conf/conf.php'))
-{
-    die("You need a conf.php, bro.");
-}
-// REQUIRE!
-$config = include 'conf/conf.php';
+// Grab our config array
+$config = get_config();
 
-// Set our last run success timestamp for 0 so it announces any success results immediatley.
-$last_run_success = 0;
-// Set our last run success timestamp for 0 so it announces any failure results immediatley.
-$last_run_failure = 0;
+$last_run = array(
+    // Set our last run success timestamp for 0 so it announces any success results immediatley.
+    'success'   =>  0,
+    // Set our last run failure timestamp for 0 so it announces any failure results immediatley.
+    'failure'   =>  0,
+);
 
 // DO ETTTT ALWAYS
 while(true)
 {
+    // We need to have some services configured to do anything... and that should be an array
     if(isset($config['services']) && is_array($config['services']))
     {
-        $alerts = array();
+        // Get any alerts on status --- irregular responses or missing services
+        $status_alerts = get_status_alerts();
 
-        foreach ($config['services'] as $service => $service_url)
+        // If we have alerts turned on
+        if(! empty($config['alerts']))
         {
-            $status = API_Thingy::status($service_url);
-            switch ($status)
+            // If we didn't get any status alerts
+            if (empty($status_alerts))
             {
-                case 200:
-                    break;
-                case 400:
-                    // Swallow bad request since we'll get that with non-authed calls
-                    break;
-                case 401:
-                    // Swallow Unauthorized as we'll get that with non-auth calls
-                    break;
-                case 403:
-                    $alerts[] = "{$service} is in the forbidden zone.";
-                    break;
-                case 404:
-                    $alerts[] = "{$service} is not found.";
-                    break;
-                case 500:
-                    $alerts[] = "{$service} is broken.";
-                    break;
-                default:
-                    $alerts[] = "{$service} is acting weird.";
-                    break;
-            }
-        }
-
-        if($config['alerts'])
-        {
-            if (empty($alerts))
-            {
-                if (announcement_threshold($config['alert_frequency'], $last_run_success) || ($last_run_failure > $last_run_success))
+                // If we have met the announcement threshold, we can make announcements OR we are changing status
+                if (announcement_threshold($last_run['success']) || ($last_run['failure'] > $last_run['success']))
                 {
-                    // We made it without any failures!
-                    exec('afplay "' . $config['alert_success'] . '"');
-
-                    if ($last_run_failure > $last_run_success)
+                    // Check for audio alerts
+                    if (in_array('audio', $config['alerts']))
                     {
-                        exec('say "' . $config['alert_restored'] . '"');
+                        // Fire audio alerts
+                        audio_success($last_run);
                     }
 
                     // reset our last run timestamp
-                    $last_run_success = strtotime('now');
+                    $last_run['success'] = strtotime('now');
                 }
             }
             else
             {
-                if (announcement_threshold($config['alert_frequency'], $last_run_failure) || ($last_run_success > $last_run_failure))
+                // If we have met the announcement threshold, we can make announcements OR we are changing status
+                if (announcement_threshold($last_run['failure']) || ($last_run['success'] > $last_run['failure']))
                 {
-                    // We had failures!
-                    exec('afplay "' . $config['alert_failure'] . '"');
-                    if(is_array($alerts))
+                    // Check for audio alerts
+                    if (in_array('audio', $config['alerts']))
                     {
-                        foreach($alerts as $alert)
-                        {
-                            exec('say "' . $alert . '"');
-                        }
+                        // Fire audio alerts
+                        audio_failure($status_alerts);
                     }
+
+                    // Check for email alerts
+                    if (in_array('email', $config['alerts']))
+                    {
+                        // Fire email alerts
+                        send_email($status_alerts);
+                    }
+
                     // reset our last run timestamp
-                    $last_run_failure = strtotime('now');
+                    $last_run['failure'] = strtotime('now');
                 }
             }
         }
     }
 
+    // Sleep until next status check
     sleep($config['status_check_frequency']);
 }
 
-function announcement_threshold($interval, $last_run)
+/**
+ * determine if the announcement threshold has been met
+ *
+ * @param $timestamp
+ * @return bool
+ */
+function announcement_threshold($timestamp)
 {
-    return ((strtotime('now') - $interval) > $last_run) ? true : false;
+    // Grab our config array
+    $config = get_config();
+
+    // return a bool depending on if we've hit our announcement frequency threshold
+    return ((strtotime('now') - $config['alert_frequency']) > $timestamp) ? true : false;
+}
+
+/**
+ * Send alert emails on failure status(es)
+ *
+ * @param Array $alerts
+ */
+function send_email($alerts = array())
+{
+    // Grab our config array
+    $config = get_config();
+
+    // Construct a message with each alert on a new line
+    $message = implode("\n", $alerts);
+
+    // Grab our to address from the conf
+    $to = $config['alert_email']['to'];
+
+    // Grab our subject from the conf and add a date time string to it for unqieness
+    $subject = $config['alert_email']['subject'] . " " . get_formatted_datetime();
+
+    $headers = 'From: so-gnar@no-reply' . "\r\n" .
+        'Reply-To: so-gnar@no-reply' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    // Send the message with provided details
+    mail($to, $subject, $message, $headers);
+}
+
+/**
+ * Gets formatted datetime of NOW
+ *
+ * @return string
+ */
+function get_formatted_datetime()
+{
+    // Grab our config array
+    $config = get_config();
+
+    // Start DateTime, set it to configured DateTimeZone, and format it how I want it.
+    $now = new DateTime();
+    $now->setTimezone(new DateTimeZone($config['timezone']));
+    return $now->format('m/d/y g:i a');
+}
+
+/**
+ * grab the config as array
+ *
+ * @return mixed
+ */
+function get_config()
+{
+    // CONF
+    if (! file_exists('conf/conf.php'))
+    {
+        die("You need a conf.php, bro.");
+    }
+
+    // load that conf array up and return it!
+    return include 'conf/conf.php';
+}
+
+/**
+ * run the audio failure commands
+ *
+ * @param array $status_alerts
+ */
+function audio_failure($status_alerts = array())
+{
+    // Grab our config array
+    $config = get_config();
+
+    // We had failures!
+    exec('afplay "' . $config['alert_failure'] . '"');
+    if(is_array($status_alerts))
+    {
+        foreach($status_alerts as $status_alert)
+        {
+            exec('say "' . $status_alert . '"');
+        }
+    }
+}
+
+/**
+ * run the audio success commands
+ *
+ * @param $last_run
+ */
+function audio_success($last_run)
+{
+    // Grab our config array
+    $config = get_config();
+
+    // We made it without any failures!
+    exec('afplay "' . $config['alert_success'] . '"');
+
+    if ($last_run['failure'] > $last_run['success'])
+    {
+        exec('say "' . $config['alert_restored'] . '"');
+    }
+}
+
+/**
+ * loop through our setup services and sniff for status alerts
+ *
+ * @return array
+ */
+function get_status_alerts()
+{
+    // Grab our config array
+    $config = get_config();
+
+    // setup our alerts array
+    $alerts = array();
+
+    // loop thourhg our defined services and check status
+    foreach ($config['services'] as $service => $service_url)
+    {
+        $status = API_Thingy::status($service_url);
+        switch ($status)
+        {
+            case 200:
+                break;
+            case 400:
+                // Swallow bad request since we'll get that with non-authed calls
+                break;
+            case 401:
+                // Swallow Unauthorized as we'll get that with non-auth calls
+                break;
+            case 403:
+                $alerts[] = "{$service} is in the forbidden zone.";
+                break;
+            case 404:
+                $alerts[] = "{$service} is not found.";
+                break;
+            case 500:
+                $alerts[] = "{$service} is broken.";
+                break;
+            default:
+                $alerts[] = "{$service} is acting weird.";
+                break;
+        }
+    }
+
+    return $alerts;
 }
